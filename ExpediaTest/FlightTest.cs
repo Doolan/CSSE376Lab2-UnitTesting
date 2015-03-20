f81470e2366ac5fcc8c9d03aa1a454495a3cd6c0
Doolan <doolansr@rose-hulman.edu>
2015-03-20 17:14:31 -0400

    Completed the Lab

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -8,5 +8,56 @@ namespace ExpediaTest
 	public class FlightTest
 	{
 		//TODO Task 7 items go here
+        private readonly DateTime startDate = DateTime.Today;
+        private readonly DateTime endDate = DateTime.Today.AddDays(5.4);
+        private readonly int mileCount = 5;
+
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(startDate, endDate, mileCount);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForOneDay()
+        {
+            var target = new Flight(startDate, startDate.AddDays(1), mileCount);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTwoDays()
+        {
+            var target = new Flight(startDate, startDate.AddDays(2), mileCount);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+        [
+        Test()]
+        public void TestThatFlightHasCorrectBasePriceForTenDays()
+        {
+            var target = new Flight(startDate, startDate.AddDays(10), mileCount);
+            Assert.AreEqual(400, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightThrowsOnBadDates()
+        {
+            var target = new Flight(endDate, startDate, mileCount);
+        }
+
+        [Test()]
+        public void TestThatFlightDoesntThrowsOnSameDate()
+        {
+            var target = new Flight(startDate, startDate, mileCount);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightThrowsOnBadLength()
+        {
+            var target = new Flight(startDate, endDate, -25);
+        }
 	}
 }
